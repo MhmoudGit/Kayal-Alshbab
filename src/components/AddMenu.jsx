7bdc6c6b5289fde42aaab433497bb103a4531aea
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-11 14:48:53 +0200

    AddMenu

--- src/components/AddMenu.jsx
+++ src/components/AddMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AiOutlineArrowRight } from 'react-icons/ai';
-function AddMenu() {
+function AddMenu({close}) {
 return (
-    <div className= "fixed inset-x-0 top-0 w-full h-full bg-gray-500/50 visible">
+    <div className= "fixed inset-x-0 top-0 w-full h-full backdrop-blur-sm bg-gray/30">
         <div className="flex flex-col gap-4  bg-white p-10  shadow-xl w-full md:w-[50%]  fixed right-0 top-0 z-40 h-full">
-                <button className="flex gap-2 items-center text-3xl font-extrabold bg-transparent border-none ">
+                <button className="flex gap-2 items-center text-3xl font-extrabold bg-transparent border-none "
+                onClick={close}
+                >
                     <AiOutlineArrowRight/> 
                     <h1>إضافة قائمة</h1>
                 </button>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai'
+import AddMenu from '../components/AddMenu';
 
 const Menu = () => {
+  const [addMenu,setaddMenu]=useState(false);
   return (
     <div className="lg:w-4/5">
+      {addMenu&&<AddMenu close={()=>{setaddMenu(false)}}/>}
+      {/* <AddMenu/> */}
       <hr className="hidden lg:block lg:my-10" />
       <div className="py-2 px-3 lg:px-10 lg:mt-11">
         <h1 className="text-2xl lg:text-4xl font-bold mx-3 mt-3">
@@ -17,7 +22,9 @@ const Menu = () => {
         p-2 lg:p-10 w-full rounded-full 
         text-lg lg:text-xl font-bold text-white my-5 
         lg:rounded-md lg:text-blue-500 lg:border lg:border-blue-500 
-        lg:hover:bg-blue-500 lg:hover:text-white duration-300 flex justify-center items-center">
+        lg:hover:bg-blue-500 lg:hover:text-white duration-300 flex justify-center items-center"
+        onClick={()=>{setaddMenu(true)}}
+        >
           <AiOutlinePlus className='text-md mx-1'/>
           اضف قائمة
         </button>
