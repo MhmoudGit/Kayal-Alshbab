859e19829d90aa6cf011ae901acf7f6cf7f734b2
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-12 17:49:57 +0200

    Menu

--- src/components/Listitems.jsx
+++ src/components/Listitems.jsx
@@ -25,7 +25,7 @@ const Listitems = ({ data, id, dlt }) => {
           <div
             className={`${
               menu ? "block" : "hidden"
-            } absolute bg-white py-1 left-3 m-2 text-sm shadow-md z-10`}
+            } absolute bg-white py-1 left-3 m-2 text-sm shadow-md z-10`} 
           >
             <p className="p-2 px-6 cursor-pointer hover:bg-blue-800 hover:text-white">تعديل</p>
             <p className="p-2 px-6 cursor-pointer hover:bg-blue-800 text-red-600 hover:text-white" onClick={() => dlt(id)}>مسح</p>
