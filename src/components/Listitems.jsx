521491b9c30c6ebea8e992e7cb8b6bb547e87374
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-12 17:43:06 +0200

    addItem

--- src/components/Listitems.jsx
+++ src/components/Listitems.jsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import AddItem from "./AddItem";
 
 const Listitems = ({ data, id, dlt }) => {
   const [list, setList] = useState(false);
   const [menu, setmenu] = useState(false);
- 
-  
+  const [additem, setaddItem] = useState(false);
+  const open = () => {
+    setaddItem(true);
+  };
   return (
     <div className="border my-4 mx-6 rounded-lg ">
+      {additem && (
+        <AddItem
+          close={() => {
+            setaddItem(false);
+          }}
+          categoryId={id}
+        />
+      )}
       <div className="flex items-center">
         <p
           className="bg-white text-xl cursor-pointer py-5 px-3 w-full flex items-center"
@@ -34,7 +45,7 @@ const Listitems = ({ data, id, dlt }) => {
       {list && (
         <div>
           <p className="bg-white text-xl p-10">{data.title}</p>
-          <button className="text-sm border rounded-full p-4 text-blue-500 flex justify-center m-4">
+          <button className="text-sm border rounded-full p-4 text-blue-500 flex justify-center m-4" onClick={open}>
             <AiOutlinePlus className="mx-1"/> اضف صنف
           </button>
         </div>
