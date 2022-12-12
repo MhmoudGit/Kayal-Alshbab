547fb2d1789e8aeb278aa292c3b1e8e9a1572444
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-12 17:53:06 +0200

    menu

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div
       dir="rtl"
-      className="flex flex-col lg:flex-row text-indigo-900  bg-gray-50 min-h-[100vh]"
+      className="flex flex-col lg:flex-row text-indigo-900  bg-gray-50 min-h-[100vh] select-none"
     >
       <Router>
         <Navbar />
