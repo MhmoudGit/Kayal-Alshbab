32a4d6a5bd4fa1c744dd417e6ac6e025dfc8a03c
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-14 12:31:49 +0200

    list

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,7 @@ import { BiPencil, BiQrScan, BiRestaurant, BiTrendingUp } from "react-icons/bi";
 
 export const navLinks = [
   { id: 1, name: "لوحة التحكم", route: "/dashboard", icon: <BiTrendingUp /> },
-  { id: 2, name: "المطعم", route: "/restaurant", icon: <BiRestaurant /> },
+  // { id: 2, name: "المطعم", route: "/restaurant", icon: <BiRestaurant /> },
   { id: 3, name: "القائمة", route: "/", icon: <BiPencil /> },
   { id: 4, name: "الكيو ار كود", route: "/qr-code", icon: <BiQrScan /> },
 ];
