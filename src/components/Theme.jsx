259df40bcf1d6ca08b92eca8f411205df139f269
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-20 13:06:28 +0200

    settings

---
+++ src/components/Theme.jsx
@@ -0,0 +1,9 @@
+const Theme = () => {
+    return (
+        <div className=' p-10 bg-white mx-5 lg:mx-12 rounded-md'>
+        <h2 className="font-bold text-2xl">خيارات المظهر</h2>
+        </div>
+    )
+}
+
+export default Theme
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,22 @@
+import Theme from '../components/Theme'
+
 const Settings = () => {
-  return (
-    <div className="lg:w-5/6 select-non max-h-[70vh] lg:max-h-[100vh]  overflow-y-auto">
-      <hr className="hidden lg:block lg:my-10" />
-      <div className="py-2 px-3 lg:px-10 lg:mt-11">
-        <h2 className="text-2xl lg:text-4xl font-bold mx-3 mt-3">الإعدادات</h2>
-      </div>
-    </div>
-  )
+
+	return (
+		<div className='lg:w-5/6 select-non max-h-[70vh] lg:max-h-[100vh]  overflow-y-auto'>
+			<hr className='hidden lg:block lg:my-10' />
+			<div className='py-2 px-3 lg:px-10 lg:mt-11'>
+				<h2 className='text-2xl lg:text-4xl font-bold mx-3 mt-3'>الإعدادات</h2>
+			</div>
+			<div className='my-2 border-b mx-12 flex gap-8'>
+				<p className='text-lg py-3 text-blue-500 w-fit font-semibold border-b-2 border-blue-500'>
+					ادارة المستخدمين
+				</p>
+				<p className='text-lg py-3 w-fit'>المظهر</p>
+			</div>
+      <Theme />
+		</div>
+	)
 }
 
 export default Settings
