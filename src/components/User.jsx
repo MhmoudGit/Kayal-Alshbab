61932f8e2dab3806a01622f101a8254ca61ae58f
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-21 11:33:21 +0200

    settings

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,9 @@
-const Theme = () => {
+const User = () => {
 	return (
 		<div className=' p-10 bg-white mx-5 lg:mx-12 rounded-md'>
-			
+			<h2 className='font-bold text-2xl'>المستخدمون</h2>
 		</div>
 	)
 }
 
-export default Theme
+export default User
