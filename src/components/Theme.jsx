60a43fbf2d7ee2f4bce51dcf0a9f62ea86db7bea
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-21 12:07:26 +0200

    settings

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -2,11 +2,11 @@ const Theme = () => {
 	return (
 		<div className='py-6 px-10 bg-white mx-5 lg:mx-12 rounded-md shadow-md'>
 			<h2 className='font-bold text-2xl'>خيارات المظهر</h2>
-            <p className="font-bold text-xl mt-7">النمط</p>
-            <div className="py-8">
-                <img src='Capture.PNG' alt="theme1" className="w-44"/>
-				<p className="p-3">افتراضي</p>
-            </div>
+			<p className='font-bold text-xl mt-7'>النمط</p>
+			<div className='py-8'>
+				<img src='Capture.PNG' alt='theme1' className='w-44' />
+				<p className='p-3'>افتراضي</p>
+			</div>
 		</div>
 	)
 }
