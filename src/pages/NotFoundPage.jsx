6ad91dc566033e8d69eafef3d20b7b373a197f26
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2022-12-22 18:03:32 +0200

    notfound

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import image from '../img/not found.jpg'
 function NotFoundPage() {
-    return (
-        <div className='w-full bg-cyan-50'>
-            <div className='max-w-4xl mx-auto p-3'>
-            <img src={image} className="mx-auto"/>
-            </div>
-        </div>
-    )
+	return (
+		<div className='absolute h-full w-full bg-sky-50 z-50'>
+			<img src={image} className='h-full w-full' />
+		</div>
+	)
 }
 
 export default NotFoundPage
